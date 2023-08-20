abccd3bb991368e740a2d48c930bbd9e08ee8bb1
Elaidzha1940 <eli080293@gmail.com>
2023-08-20 18:15:30 +0300

    WeatherManager

--- WeatherLoacationApp/Managers/WeatherManager.swift
+++ WeatherLoacationApp/Managers/WeatherManager.swift
@@ -15,8 +15,10 @@ import CoreLocation
 
 class WeatherManager {
     
-    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
         
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("727faf1bd3a72565b4c76a30128f3371")&units=metric") else { fatalError("Missing URL")}
         
+        let urlRequest = URLRequest 
     }
 }
