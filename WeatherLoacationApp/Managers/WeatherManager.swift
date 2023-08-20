c111ec5f9fc93a394c6975333c19e3cbfc2f6d90
Elaidzha1940 <eli080293@gmail.com>
2023-08-20 18:06:27 +0300

    added WeatherManager

---
+++ WeatherLoacationApp/Managers/WeatherManager.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: WeatherLoacationApp
+//  File: WeatherManager.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 20.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import Foundation
+import CoreLocation
+//727faf1bd3a72565b4c76a30128f3371
+
+class WeatherManager {
+    
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        
+        
+    }
+}
