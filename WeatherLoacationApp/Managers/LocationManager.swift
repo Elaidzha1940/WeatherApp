b64c9a3cedc7b81e57a7bd8d30f4304689414eb0
Elaidzha1940 <eli080293@gmail.com>
2023-08-20 15:55:31 +0300

    added a new file / LocationManager

--- WeatherLoacationApp/Managers/LocationManager.swift
+++ WeatherLoacationApp/Managers/LocationManager.swift
@@ -29,4 +29,14 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         isLoading = true
         manager.requestLocation()
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
 }
