ac875c68f9b4c1d6c79736889de8fb9b3705fdf9
Elaidzha1940 <eli080293@gmail.com>
2023-08-20 15:11:03 +0300

    WeatherLoacationApp

---
+++ WeatherLoacationApp/WeatherLoacationAppApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: WeatherLoacationApp
+//  File: WeatherLoacationAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 20.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct WeatherLoacationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
