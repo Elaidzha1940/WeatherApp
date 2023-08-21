7780f57ada007a8d7cb57834a24abedcf7203c01
Elaidzha1940 <eli080293@gmail.com>
2023-08-21 13:45:29 +0300

    added WeatherLoacationApp

--- WeatherLoacationApp/Views/ContentView.swift
+++ WeatherLoacationApp/Views/ContentView.swift
@@ -5,7 +5,7 @@
 //  Created by: Elaidzha Shchukin
 //  Date: 20.08.2023
 //
-//  
+//  Status: in progress | Decorated
 //
 //  */
 
---
+++ WeatherLoacationApp/Views/WeatherView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: WeatherLoacationApp
+//  File: WeatherView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 21.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
