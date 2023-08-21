175414e3cc88b3268ae56a7db322348ee7ec5ab7
Elaidzha1940 <eli080293@gmail.com>
2023-08-21 13:48:49 +0300

    added WeatherLoacationApp

---
+++ WeatherLoacationApp/Preview Content/ModelData.swift
@@ -0,0 +1,12 @@
+//  /*
+//
+//  Project: WeatherLoacationApp
+//  File: ModelData.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 21.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import Foundation
